1ae7af35617c1a730bf62b2b8587cf9e739f3b4d
Admin <admin@mac-admin.local>
2016-08-20 18:03:04 +0600

    Initial Commit

---
+++ test_iOS_intern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_iOS_intern
+//
+//  Created by Admin on 20.08.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_iOS_intern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_iOS_intern
+//
+//  Created by Admin on 20.08.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
